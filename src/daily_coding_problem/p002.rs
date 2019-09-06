0c5dbf567c3a9f4a4b1f1f8f0fa7b4508bfd4a21
JP-Ellis <josh@jpellis.me>
2019-09-06 16:33:18 +1000

    Solve DCP2
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/daily_coding_problem/p002.rs
+++ src/daily_coding_problem/p002.rs
@@ -12,12 +12,60 @@ be `[2, 3, 6]`.
 
 Follow-up: what if you can't use division?"#;
 
+fn products_except(list: &[i64]) -> Vec<i64> {
+    let mut result = Vec::with_capacity(list.len());
+    for i in 0..list.len() {
+        result.push(
+            list[0..i].iter().product::<i64>() * list[i + 1..list.len()].iter().product::<i64>(),
+        )
+    }
+    result
+}
+
 impl Problem for P {
     fn statement(&self) {
         println!("{}", STATEMENT);
     }
 
     fn solve(&self) -> Result<(), String> {
-        Err("not implemented".to_string())
+        for list in &[&[1, 2, 3, 4, 5][..], &[-5, 0, 2], &[2, 1, 2]] {
+            println!("{:?} => {:?}", list, products_except(list));
+        }
+
+        if products_except(&[1, 2, 3, 4, 5]) == vec![120, 60, 40, 30, 24]
+            && products_except(&[3, 2, 1]) == vec![2, 3, 6]
+        {
+            Ok(())
+        } else {
+            Err(format!(
+                "Did not get expected result.\n  {:?} != {:?}\n  {:?} != {:?}",
+                products_except(&[1, 2, 3, 4, 5]),
+                vec![120, 60, 40, 30, 24],
+                products_except(&[3, 2, 1]),
+                vec![2, 3, 6]
+            ))
+        }
     }
+}
+
+#[cfg(all(test, feature = "nightly"))]
+mod benches {
+    use test::Bencher;
+
+    macro_rules! bench {
+        ($f:ident, $n:expr) => {
+            #[bench]
+            fn $f(b: &mut Bencher) {
+                let v: Vec<_> = (0..$n).collect();
+
+                b.iter(|| test::black_box(super::products_except(&v)));
+            }
+        };
+    }
+
+    bench!(n_0004, 4);
+    bench!(n_0016, 16);
+    bench!(n_0064, 64);
+    bench!(n_0256, 256);
+    bench!(n_1024, 1024);
 }
