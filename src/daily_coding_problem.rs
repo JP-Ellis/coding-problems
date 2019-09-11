1a1e9f8de0d6e25e51c065758a8ab914cc8b1e23
JP-Ellis <josh@jpellis.me>
2019-09-11 11:18:46 +1000

    Add DCP17, DCP18, DCP19

--- src/daily_coding_problem.rs
+++ src/daily_coding_problem.rs
@@ -19,6 +19,9 @@ mod p013;
 mod p014;
 mod p015;
 mod p016;
+mod p017;
+mod p018;
+mod p019;
 
 pub fn problem(i: usize) -> Result<Box<dyn Problem>, String> {
     match i {
@@ -38,6 +41,19 @@ pub fn problem(i: usize) -> Result<Box<dyn Problem>, String> {
         14 => Ok(Box::new(p014::P)),
         15 => Ok(Box::new(p015::P)),
         16 => Ok(Box::new(p016::P)),
+        17 => Ok(Box::new(p017::P)),
+        18 => Ok(Box::new(p018::P)),
+        19 => Ok(Box::new(p019::P)),
+        // 20 => Ok(Box::new(p020::P)),
+        // 21 => Ok(Box::new(p021::P)),
+        // 22 => Ok(Box::new(p022::P)),
+        // 23 => Ok(Box::new(p023::P)),
+        // 24 => Ok(Box::new(p024::P)),
+        // 25 => Ok(Box::new(p025::P)),
+        // 26 => Ok(Box::new(p026::P)),
+        // 27 => Ok(Box::new(p027::P)),
+        // 28 => Ok(Box::new(p028::P)),
+        // 29 => Ok(Box::new(p029::P)),
         i => Err(format!(
             "Problem '{}' is not knowing within Daily Coding Problems.",
             i
---
+++ src/daily_coding_problem/p019.rs
@@ -0,0 +1,26 @@
+use crate::Problem;
+
+pub struct P;
+
+const STATEMENT: &str = r#"A builder is looking to build a row of N houses that
+can be of K different colors. He has a goal of minimizing cost while ensuring
+that no two neighboring houses are of the same color.
+
+Given an N by K matrix where the nth row and kth column represents the cost to
+build the nth house with kth color, return the minimum cost which achieves this
+goal."#;
+
+impl Problem for P {
+    fn statement(&self) {
+        println!("{}", STATEMENT);
+    }
+
+    fn solve(&self) -> Result<(), String> {
+        Err("not implemented".to_string())
+    }
+}
+
+#[cfg(all(test, feature = "nightly"))]
+mod benches {
+    use test::Bencher;
+}
