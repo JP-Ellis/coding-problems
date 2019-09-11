05990328e2f9c7dea2572a70d9eb673a47cab18c
JP-Ellis <josh@jpellis.me>
2019-09-11 11:18:31 +1000

    Solve DCP4

--- src/daily_coding_problem/p004.rs
+++ src/daily_coding_problem/p004.rs
@@ -12,17 +12,49 @@ should give `3`.
 
 You can modify the input array in-place."#;
 
+fn find_missing(list: &mut [i64]) -> i64 {
+    // Sort the array and get the index of the 1 (or where the one should be)
+    list.sort_unstable();
+    let start = match list.binary_search(&1) {
+        Ok(i) => i,
+        Err(i) => i,
+    };
+
+    let mut n = 0;
+    for &ni in &list[start..] {
+        if ni == n {
+            continue;
+        } else if ni == n + 1 {
+            n += 1;
+            continue;
+        }
+        break;
+    }
+    n + 1
+}
+
 impl Problem for P {
     fn statement(&self) {
         println!("{}", STATEMENT);
     }
 
     fn solve(&self) -> Result<(), String> {
-        Err("not implemented".to_string())
-    }
-}
+        for list in &mut [
+            &mut [3, 4, -1, 1][..],
+            &mut [1, 2, 0],
+            &mut [-1, 0, 1, 2, 3],
+            &mut [2, -1, -1],
+            &mut [1, 1, 2, 3, 4, 3, 5, 3],
+            &mut [],
+        ] {
+            print!("List: {:?} => ", list);
+            println!("{}", find_missing(list));
+        }
 
-#[cfg(all(test, feature = "nightly"))]
-mod benches {
-    use test::Bencher;
+        if find_missing(&mut [3, 4, -1, 1]) == 2 && find_missing(&mut [1, 2, 0]) == 3 {
+            Ok(())
+        } else {
+            Err("Incorrectly found the correct missing integer.".to_string())
+        }
+    }
 }
