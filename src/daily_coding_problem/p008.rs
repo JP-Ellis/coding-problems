03b68fe0eddba26f6c31f8d59ef3dbb6dcd05d17
JP-Ellis <josh@jpellis.me>
2019-09-14 11:37:53 +1000

    Solve DCP8
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/daily_coding_problem/p008.rs
+++ src/daily_coding_problem/p008.rs
@@ -22,13 +22,87 @@ For example, the following tree has 5 unival subtrees:
 ```
 "#;
 
+struct Node<T> {
+    val: T,
+    left: Option<Box<Node<T>>>,
+    right: Option<Box<Node<T>>>,
+}
+
+impl<T> Node<T> {
+    fn leaf(val: T) -> Self {
+        Node {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+impl<T: Eq> Node<T> {
+    fn count_universal_subtrees(&self) -> usize {
+        match (&self.left, &self.right) {
+            (None, None) => 1,
+            (Some(t), None) | (None, Some(t)) => {
+                t.count_universal_subtrees() + if t.val == self.val { 1 } else { 0 }
+            }
+            (Some(l), Some(r)) => {
+                l.count_universal_subtrees()
+                    + r.count_universal_subtrees()
+                    + if l.val == self.val && r.val == self.val {
+                        1
+                    } else {
+                        0
+                    }
+            }
+        }
+    }
+}
+
 impl Problem for P {
     fn statement(&self) {
         println!("{}", STATEMENT);
     }
 
     fn solve(&self) -> Result<(), String> {
-        Err("not implemented".to_string())
+        let tree0 = Node::leaf(true);
+        let tree1 = Node {
+            val: true,
+            left: Some(Box::new(Node::leaf(true))),
+            right: Some(Box::new(Node::leaf(true))),
+        };
+        let tree2 = Node {
+            val: false,
+            left: Some(Box::new(Node {
+                val: true,
+                left: Some(Box::new(Node::leaf(true))),
+                right: Some(Box::new(Node::leaf(true))),
+            })),
+            right: Some(Box::new(Node::leaf(false))),
+        };
+        let tree3 = Node {
+            val: false,
+            left: Some(Box::new(Node::leaf(true))),
+            right: Some(Box::new(Node {
+                val: false,
+                left: Some(Box::new(Node {
+                    val: true,
+                    left: Some(Box::new(Node::leaf(true))),
+                    right: Some(Box::new(Node::leaf(true))),
+                })),
+                right: Some(Box::new(Node::leaf(false))),
+            })),
+        };
+
+        println!("Tree 0: {}", tree0.count_universal_subtrees());
+        println!("Tree 1: {}", tree1.count_universal_subtrees());
+        println!("Tree 2: {}", tree2.count_universal_subtrees());
+        println!("Tree 3: {}", tree3.count_universal_subtrees());
+
+        if tree3.count_universal_subtrees() == 5 {
+            Ok(())
+        } else {
+            Err("Did not calculated the correct number of subtrees.".to_string())
+        }
     }
 }
 
