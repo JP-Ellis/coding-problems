082e0f65e17095281fea2f6fea036dbc6276dd22
JP-Ellis <josh@jpellis.me>
2019-09-14 12:58:36 +1000

    Solve DCP9
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/daily_coding_problem/p009.rs
+++ src/daily_coding_problem/p009.rs
@@ -12,13 +12,50 @@ For example, `[2, 4, 6, 2, 5]` should return `13`, since we pick `2`, `6`, and
 
 Follow-up: Can you do this in O(N) time and constant space?"#;
 
+/// Calculate the largest sum of non-adjacent elements in the list.
+///
+/// If the list is empty, 0 is returned.  If the list consists of purely
+/// non-positive integers, the largest element will be returned.
+fn max_sum(l: &[i64]) -> i64 {
+    match l.len() {
+        0 => 0,
+        1 => l[0],
+        2 => *l.iter().max().unwrap(),
+        3 => *[l[0], l[0] + l[2], l[1], l[2]].iter().max().unwrap(),
+        _4_or_more => {
+            let s0 = max_sum(&l[2..]);
+            let s1 = max_sum(&l[3..]);
+            *[l[0], l[0] + s0, s0, l[1], l[1] + s1, s1]
+                .iter()
+                .max()
+                .unwrap()
+        }
+    }
+}
+
 impl Problem for P {
     fn statement(&self) {
         println!("{}", STATEMENT);
     }
 
     fn solve(&self) -> Result<(), String> {
-        Err("not implemented".to_string())
+        for list in &[
+            &[1, 5, 3, 4, 2][..],
+            &[2, 6, 0],
+            &[4, -3, 2],
+            &[-5, -2, -3, -1],
+            &[-1, -1, 9],
+            &[2, 4, 6, 2, 5],
+            &[5, 1, 1, 5],
+        ] {
+            println!("{:?} => {}", list, max_sum(list));
+        }
+
+        if max_sum(&[2, 4, 6, 2, 5]) == 13 && max_sum(&[5, 1, 1, 5]) == 10 {
+            Ok(())
+        } else {
+            Err("Did not get the desired sum.".to_string())
+        }
     }
 }
 
