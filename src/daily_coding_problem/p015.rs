53af4d455c16c0ee1a59c7ed8c85952597767cb8
JP-Ellis <josh@jpellis.me>
2019-09-27 01:05:24 +1000

    Solve DCP15
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/daily_coding_problem/p015.rs
+++ src/daily_coding_problem/p015.rs
@@ -1,33 +1,37 @@
 use crate::{Error, Problem};
+use rand::prelude::*;
 use std::io::prelude::*;
-use std::ops;
 
 pub struct P;
 
 const STATEMENT: &str = r#"Given a stream of elements too large to store in
 memory, pick a random element from the stream with uniform probability."#;
 
-struct InfinitCounter<T> {
-    val: T,
-    step: T,
-}
+fn sample_stream<T, I>(s: I, n: usize) -> Vec<T>
+where
+    I: IntoIterator<Item = T>,
+{
+    assert!(
+        n > 0,
+        "The number of samples to return must be greater than 1."
+    );
+    let mut result = Vec::new();
+    let rng = &mut rand::thread_rng();
+    let dist = rand::distributions::Uniform::new(0, n);
 
-impl<T: num::Zero + num::One> Default for InfinitCounter<T> {
-    fn default() -> Self {
-        InfinitCounter {
-            val: T::zero(),
-            step: T::one(),
+    for (i, si) in s.into_iter().enumerate() {
+        if i < n {
+            if i == 0 {
+                result.push(si);
+            } else {
+                result.insert(rng.gen_range(0, result.len()), si);
+            }
+        } else if rng.gen::<f64>() < (n as f64) / (i as f64) {
+            result[dist.sample(rng)] = si;
         }
     }
-}
 
-impl<T: num::One + ops::Add<Output = T> + Copy> Iterator for InfinitCounter<T> {
-    type Item = T;
-
-    fn next(&mut self) -> Option<T> {
-        self.val = self.val + self.step;
-        Some(self.val)
-    }
+    result
 }
 
 impl Problem for P {
@@ -39,8 +43,47 @@ impl Problem for P {
         STATEMENT
     }
 
-    fn solve(&self, _out: &mut dyn Write) -> Result<(), Error> {
-        Err(())?
+    fn solve(&self, out: &mut dyn Write) -> Result<(), Error> {
+        for &n in &[1, 2, 5, 10] {
+            writeln!(
+                out,
+                "Random {:>2}-sample for 0..10   : {:?}",
+                n,
+                sample_stream(0..10, n)
+            )
+            .unwrap();
+            writeln!(
+                out,
+                "Random {:>2}-sample for 0..100  : {:?}",
+                n,
+                sample_stream(0..100, n)
+            )
+            .unwrap();
+            writeln!(
+                out,
+                "Random {:>2}-sample for 0..1000 : {:?}",
+                n,
+                sample_stream(0..1000, n)
+            )
+            .unwrap();
+        }
+
+        let mut mean = 0.0;
+        for _ in 0..1_000 {
+            let sum: usize = sample_stream(0..1_000, 10).iter().sum();
+            mean += sum as f64 / 1e3;
+        }
+
+        let expected = 5e3;
+        let std_dev = 30.0;
+        if expected - 2.0 * std_dev < mean && mean < expected + 2.0 * std_dev {
+            Ok(())
+        } else {
+            Err(format!(
+                "[random] Expected mean: {:.3e}.  Got {:.3e}.",
+                expected, mean
+            ))?
+        }
     }
 }
 
