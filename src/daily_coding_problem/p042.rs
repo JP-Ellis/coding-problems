1d119882db841edb52e059e1000fda5194c48df9
JP-Ellis <josh@jpellis.me>
2019-10-04 13:25:02 +1000

    Solve DCP42
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/daily_coding_problem/p042.rs
+++ src/daily_coding_problem/p042.rs
@@ -1,5 +1,5 @@
 use crate::{Error, Problem};
-use std::io::prelude::*;
+use std::{cmp::Ordering, io::prelude::*};
 
 pub struct P;
 
@@ -13,6 +13,27 @@ the list are positive.
 For example, given S = [12, 1, 61, 5, 9, 2] and k = 24, return [12, 9, 2, 1]
 since it sums up to 24."#;
 
+/// Given a list of integers `list`, find all subsets which sum up to `target`.
+///
+/// If no subset could be found, `None` is returned.
+fn sum_subsets(list: &[i64], target: i64) -> Vec<Vec<i64>> {
+    let mut results = Vec::new();
+    for (idx, &i) in list.iter().enumerate() {
+        match i.cmp(&target) {
+            Ordering::Less => {
+                for mut subresult in sum_subsets(&list[idx + 1..], target - i) {
+                    subresult.push(i);
+                    results.push(subresult);
+                }
+            }
+            Ordering::Equal => (results.push(vec![i])),
+            Ordering::Greater => (),
+        }
+    }
+
+    results
+}
+
 impl Problem for P {
     fn name(&self) -> &str {
         "Daily Coding Problem 42"
@@ -22,8 +43,22 @@ impl Problem for P {
         STATEMENT
     }
 
-    fn solve(&self, _out: &mut dyn Write) -> Result<(), Error> {
-        Err(())?
+    fn solve(&self, out: &mut dyn Write) -> Result<(), Error> {
+        for list in &[&[1, 2, 3, 4, 5][..], &[-2, 0, 1, 2, 5]] {
+            writeln!(out, "{:?}", list).unwrap();
+            for target in 0..=15 {
+                writeln!(out, "-> {:>2}: {:?}", target, sum_subsets(list, target)).unwrap();
+            }
+        }
+
+        let expected = vec![vec![2, 9, 1, 12]];
+        let result = sum_subsets(&[12, 1, 61, 5, 9, 2], 24);
+
+        if expected != result {
+            Err(format!("Expected {:?} but got {:?}.", expected, result))?
+        }
+
+        Ok(())
     }
 }
 
